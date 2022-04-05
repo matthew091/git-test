0a4baf39d05dac7582abe876edb69a6fc122d2d7
zm <zm9309@163.com>
2022-04-05 17:56:50 +0800

    github commit1

--- src/main/java/com/zhangmai/git/GitTest.java
+++ src/main/java/com/zhangmai/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Hello git3!");
         System.out.println("Hello master!");
         System.out.println("Hello hot-fix!");
+        System.out.println("Hello github!");
     }
 }
